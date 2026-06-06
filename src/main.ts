75eb5aac0471f54fe7e3003325bb0b48e90c4f33
mfrakhman <rrrfajar1@gmail.com>
2026-06-06 20:28:24 +0700

    feat: set global API prefix to exclude health check endpoint

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  app.setGlobalPrefix('api');
+  app.setGlobalPrefix('api', { exclude: ['health'] });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   await app.listen(process.env.PORT ?? 3000);
 }
